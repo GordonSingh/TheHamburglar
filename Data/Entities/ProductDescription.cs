cf44369636e1c7ef36a8c7d7e735b51b5d44a0ff
Arin Singh <Arin.Singh@derivco.co.za>
2016-12-07 16:43:09 +0200

    Initial Commit

---
+++ Data/Entities/ProductDescription.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Entities
+{
+    public class ProductDescription
+    {
+        public int ProductId { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        public DateTime ModifiedDateTime { get; set; }
+    }
+}
