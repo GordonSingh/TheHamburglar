646d3b07fc3c69c0f2c034d5c5d8459292f7f31e
Gordon Singh <arins@FPARISING1>
2016-12-11 13:04:57 +0200

    Working Row with Single product

---
+++ Assessment_one/Controllers/ProductController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Assessment_one.Models;
+using Data;
+
+namespace Assessment_one.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            //AdventureContext e = new AdventureContext();
+            //var allProducts = new Production
+            //{
+            //    Products = e.Products
+            //   .Where(p => p.StockNumber > p.SafetyStockNumb)
+            //   .Select(p => new ProductModel
+            //   {
+            //       ProductId = p.ProductId,
+            //       Name = p.Name,
+            //       ProductNumber = p.ProductNumber,
+            //       Color = p.Color,
+            //       StandardCost = p.StandardCost,
+            //       ReorderPoint = p.ReorderPoint,
+            //       StockNumber = p.StockNumber
+            //   }).ToList()
+            //};
+
+            var product = new Production
+            {
+              Name = "Lead Pipe",
+              Color = "Black",
+              ProductNumber = 12,
+              ReorderPoint = false,
+              StockNumber = 14,
+              StandardCost = 12.40
+            };
+
+            return View(product);
+        }
+           
+    }
+}
---
+++ Assessment_one/Models/ProductModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Data;
+using Data.Entities;
+
+namespace Assessment_one.Models
+{
+    public class ProductModel
+    {
+        public int ProductId { get; set; }
+        
+        public string Name { get; set; }
+        public int ProductNumber { get; set; }
+        public string Color { get; set; }
+        public int SafetyStockNumb { get; set; }
+        public int StockNumber { get; set; }
+        public bool ReorderPoint { get; set; }
+
+        [DataType(DataType.Currency)]
+        public double StandardCost { get; set; }
+    }
+}
